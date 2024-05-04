e8980c1c3dd6279f650f25663c67e387cc3be841
Munawar-tirta <munawar.tirta@gmail.com>
2024-05-04 21:55:19 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web-LIS Tirta Medical Indonesia</title>
+    <link rel="stylesheet" href="login2.css">
+</head>
+<body>
+  <div class="judul" >
+    
+    <img src="img/logo_tmi.jpg"  width="100" height="100">
+    <h2> Web-LIS tirta Medical Indonesia </h2>
+  </div>
+<div class="login-box">
+  <h2>Silahkan Login</h2>
+  <form>
+    <div class="user-box">
+      <input type="text" name="" required="">
+      <label>Username</label>
+    </div>
+    <div class="user-box">
+      <input type="password" name="" required="">
+      <label>Password</label>
+    </div>
+    <a href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Login
+    </a>
+  </form>
+</div>
+</body>
+</html>
